015bc3c0c6dd200d3c9d65f5a66a7542a55ba711
21120421 <nguyenchicuongk21@gmail.com>
2024-01-17 16:46:51 +0700

    fix route 2nd

--- MainSystem/routes/route.js
+++ MainSystem/routes/route.js
@@ -13,7 +13,7 @@ router.get('/', (req, res, next) => {
             controllerName = controllerName.charAt(0).toUpperCase() + controllerName.slice(1);
             const Controller = require(`../controllers/${areaName}/${controllerName}Controller`);
             const controller = new Controller();
-            controller[actionName](req, res, next);
+            controller[actionName + '_' + req.method](req, res, next);
         } else if (req.originalUrl.split('/').filter(Boolean).length == 2) {
             let index = req.originalUrl.indexOf('?');
             if (index < 0) index = req.originalUrl.length;
@@ -21,7 +21,32 @@ router.get('/', (req, res, next) => {
             controllerName = controllerName.charAt(0).toUpperCase() + controllerName.slice(1);
             const Controller = require(`../controllers/${controllerName}Controller`);
             const controller = new Controller();
-            controller[actionName](req, res, next);
+            controller[actionName + '_' + req.method](req, res, next);
+        }
+    } catch (error) {
+        next(error);
+    }
+    
+});
+
+router.post('/', (req, res, next) => {
+    try {
+        if (req.originalUrl.split('/').filter(Boolean).length == 3) {
+            let index = req.originalUrl.indexOf('?');
+            if (index < 0) index = req.originalUrl.length;
+            let [areaName, controllerName, actionName] = req.originalUrl.slice(0, index).split('/').filter(Boolean);
+            controllerName = controllerName.charAt(0).toUpperCase() + controllerName.slice(1);
+            const Controller = require(`../controllers/${areaName}/${controllerName}Controller`);
+            const controller = new Controller();
+            controller[actionName + '_' + req.method](req, res, next);
+        } else if (req.originalUrl.split('/').filter(Boolean).length == 2) {
+            let index = req.originalUrl.indexOf('?');
+            if (index < 0) index = req.originalUrl.length;
+            let [controllerName, actionName] = req.originalUrl.slice(0, index).split('/').filter(Boolean);
+            controllerName = controllerName.charAt(0).toUpperCase() + controllerName.slice(1);
+            const Controller = require(`../controllers/${controllerName}Controller`);
+            const controller = new Controller();
+            controller[actionName + '_' + req.method](req, res, next);
         }
     } catch (error) {
         next(error);
