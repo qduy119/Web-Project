f543f4f4b4d70a54c7ff22ae7b6e0aba06ed02b8
21120421 <nguyenchicuongk21@gmail.com>
2024-01-18 16:31:14 +0700

    category search

--- MainSystem/controllers/admin/CategoryController.js
+++ MainSystem/controllers/admin/CategoryController.js
@@ -3,15 +3,15 @@ const Category = require('../../models/Category');
 const pageSize = 5;
 const cloudinary = require('cloudinary').v2;
 const Joi = require('joi');
+const { Op } = require("sequelize");
 
 const schema = Joi.object({
   id: Joi.number().required(),
   title: Joi.string().required().messages({
     'string.empty': 'Tên danh mục không được để trống'
   }),
-  description: Joi.string(),
-  thumbnail: Joi.string().required().messages({
-    'any.required': 'Thumbnail không được để trống',
+  description: Joi.string().messages({
+    'string.empty': 'Mô tả không được để trống',
   })
 });
 
@@ -19,18 +19,69 @@ class CategoryController {
 
   async index_GET(req, res, next) {
     try {
-        const count = await Category.count();
+        let text = req.query.search;
+        if (!text) text = '';
+        const count = (await Category.findAll({
+          where: {
+            title: {
+              [Op.substring]: text    
+            }
+          }
+        })).length;
         const pager = { pageSize: pageSize, pages: Math.ceil(count / pageSize), curPage: 1 }
-        const categories = await Category.findAll({ limit: pageSize, offset: 0 });
+        const categories = await Category.findAll({ 
+          where: {
+            title: {
+              [Op.substring]: text    
+            }
+          },
+          limit: pageSize, 
+          offset: 0, 
+          order: [['id', 'ASC']] 
+        });
         BaseController.View(req, res, { list: categories, pager: pager });
     } catch (error) {
       next(error);
     }
   }
-  async getEntity_POST(req, res, next) {
+
+
+  async getEntity_GET(req, res, next) {
     try {
-        const { page } = req.body;
-        const categories = await Category.findAll({ limit: pageSize, offset: (page - 1) * pageSize });
+      let text = req.query.search;
+        if (!text) text = '';
+        const count = (await Category.findAll({
+          where: {
+            title: {
+              [Op.substring]: text    
+            }
+          }
+        })).length;
+        let categories;
+        const { page } = req.query;
+        const numOfPages = Math.ceil(count / pageSize);
+        if (page == numOfPages) {
+          categories = await Category.findAll({ 
+            where: {
+              title: {
+                [Op.substring]: text    
+              }
+            },
+            limit: count - (page - 1) * pageSize, 
+            offset: (page - 1) * pageSize, 
+            order: [['id', 'ASC']] });
+        }
+        else {
+          categories = await Category.findAll({
+            where: {
+              title: {
+                [Op.substring]: text    
+              }
+            }, 
+            limit: pageSize, 
+            offset: (page - 1) * pageSize, 
+            order: [['id', 'ASC']] });
+        }
         res.json({ data: categories });
     } catch (error) {
       next(error);
@@ -61,12 +112,12 @@ class CategoryController {
     try {
         const { title, description } = req.body;
         const image = req.file;
-        console.log(image);
         const maxId = await Category.max('id');
-        const err = schema.validate({id: maxId + 1, title: title, description: description, thumbnail: image?.path });
-        if (err) {
+        const value = schema.validate({id: maxId + 1, title: title, description: description });
+        
+        if (value.error) {
           if (image) cloudinary.uploader.destroy(image.filename);
-          BaseController.View(req, res, { errors: err.error.details });
+          BaseController.View(req, res, { errors: value.error.details });
           return;
         }
         
@@ -75,8 +126,54 @@ class CategoryController {
           id: maxId + 1, 
           title: title, 
           description: description, 
-          thumbnail: image.path
+          thumbnail: image?.path
         });
+        
+        res.redirect('/admin/category/index');
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async edit_GET(req, res, next) {
+    try {
+      const { id } = req.query;
+      const category = await Category.findOne({ where: { id: id } });
+      console.log(category)
+      BaseController.View(req, res, { entity: category, errors: null });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async edit_POST(req, res, next) {
+    try {
+        const { id, title, description } = req.body;
+        const image = req.file;
+        
+        const value = schema.validate({id: id, title: title, description: description });
+
+        
+        if (value.error) {
+          if (image) cloudinary.uploader.destroy(image.filename);
+          BaseController.View(req, res, { 
+            entity: { 
+              id: id, 
+              title: title, 
+              description: description
+            }, 
+            errors: value.error.details 
+          });
+          return;
+        }
+        
+        
+        await Category.update({
+          title: title,
+          description: description,
+          thumbnail: image?.path
+        }, { where: { id: id } });
+
         
         res.redirect('/admin/category/index');
     } catch (error) {
