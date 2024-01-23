49ef0f1703fd6fc13d0c2ab4a5391946a730c1a7
21120421 <nguyenchicuongk21@gmail.com>
2024-01-23 19:42:41 +0700

    finish statistic and fix product edit

--- MainSystem/controllers/admin/StatisticController.js
+++ MainSystem/controllers/admin/StatisticController.js
@@ -1,5 +1,5 @@
 const BaseController = require('./BaseController');
-const { Order } = require('../../models');
+const { Product, Order, OrderDetail } = require('../../models');
 
 class StatisticController {
 
@@ -13,11 +13,31 @@ class StatisticController {
     }
   }
 
-  async getRevenue_GET(req, res, next) {
+  async getProducts_GET(req, res, next) {
+    try {
+      const products = await Product.findAll();
+
+      return res.json({ data: products });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  async getOrders_GET(req, res, next) {
     try {
       const orderList = await Order.findAll();
 
       return res.json({ data: orderList });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getOrderDetails_GET(req, res, next) {
+    try {
+      const orderDetails = await OrderDetail.findAll();
+      return res.json({ data: orderDetails });
     } catch (error) {
       console.log(error);
     }
