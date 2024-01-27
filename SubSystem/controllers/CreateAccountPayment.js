5f599d01709e29e233becc18b7ecebaee1aab18b
21120408 <dangtuananh3012003@gmail.com>
2024-01-27 22:10:50 +0700

    fix_create_payment_account

--- SubSystem/controllers/CreateAccountPayment.js
+++ SubSystem/controllers/CreateAccountPayment.js
@@ -9,16 +9,16 @@ module.exports = class CreateAccountPayment {
         let idUser = req.body.id
         let target = db.db.query(`select * from "paymentAccount" where "userId" = $1`, [idUser])
         if(target.length !== 0) {
-            res.statusCode(200);
+            res.status(200);
             return
         }
         try{
             await db.db.none(`
                 insert into "paymentAccount"("userId") values(${idUser})
             `)
-            res.statusCode(200);
+            res.status(200);
         }catch(e) {
-            res.statusCode(404);
+            res.status(404)
         }
     }
 }
