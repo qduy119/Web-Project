b48d648d1506988b1af3c9e0615ca37d0aa8cc88
21120408 <dangtuananh3012003@gmail.com>
2024-01-27 22:16:10 +0700

    fix

--- SubSystem/controllers/CreateAccountPayment.js
+++ SubSystem/controllers/CreateAccountPayment.js
@@ -7,18 +7,18 @@ module.exports = class CreateAccountPayment {
         console.log(req.body);
         console.log("==================================")
         let idUser = req.body.id
-        let target = db.db.query(`select * from "paymentAccount" where "userId" = $1`, [idUser])
+        let target = await db.db.query(`select * from "paymentAccount" where "userId" = $1`, [idUser])
         if(target.length !== 0) {
-            res.status(200);
+            res.sendStatus(200);
             return
         }
         try{
             await db.db.none(`
                 insert into "paymentAccount"("userId") values(${idUser})
             `)
-            res.status(200);
+            res.sendStatus(200);
         }catch(e) {
-            res.status(404)
+            res.sendStatus(404)
         }
     }
 }
